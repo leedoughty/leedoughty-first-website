26924316b304abc341f8a1329945b3cdece61d52
Lee Doughty <32392044+leedoughty@users.noreply.github.com>
2018-07-19 21:14:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,75 @@
-// positioning images randomly within a div
-$(document).ready(function(){
-  // sorting out the height and width of the div
-  var position_div = $("div.container");
-  var divWidth = position_div.width();
-  var divHeight = divWidth*1;
-  position_div.height(divHeight);
-  // finding the images
-  $(".container img").each(function(){
-    var image = $(this);
-    var xPos = Math.floor(Math.random()*divWidth);
-    var yPos = Math.floor(Math.random()*divHeight);
-    image.css("top", yPos + "px");
-    image.css("left", xPos + "px");
-    var websiteURL = image.attr("sid");
+var works = [];
+var position_div;
+var divWidth;
+var divHeight;
+
+class Work {
+
+  constructor(img) {
+    this.img = img;
+    this.width = this.img.width();
+    this.height = this.img.height();
+  }
+
+  choosePosition() {
+    let count = 0;
+    var intersecting = true;
+    while (intersecting === true && count < 500 ) {
+      count++;
+      intersecting = false;
+      var xPos = Math.floor(Math.random()*(divWidth-this.width));
+      var yPos = Math.floor(Math.random()*(divHeight-this.height));
+      this.left = xPos;
+      this.right = xPos + this.width;
+      this.top = yPos;
+      this.bottom = yPos + this.height;
+      let l = works.length;
+      for (var i=0; i<l; i++) {
+        let rect = works[i];
+        if(rect!=this && this.intersect(rect)){
+          intersecting = true;
+        };
+      }
+      console.log(intersecting);
+    }
+    console.log(count);
+    this.place(xPos, yPos);
+  }
+
+  place(xPos, yPos){
+    this.x = xPos;
+    this.y = yPos;
+    this.img.css("top", yPos + "px");
+    this.img.css("left", xPos + "px");
+    var websiteURL = this.img.attr("sid");
     if (websiteURL){
-      image.click(function(){
+      this.img.click(function(){
         window.location.href = websiteURL;
       });
     }
+  }
+
+  intersect(rect) {
+      return !(rect.left > this.right ||
+               rect.right < this.left ||
+               rect.top > this.bottom ||
+               rect.bottom < this.top);
+  }
+}
+
+
+// positioning images randomly within a div
+$(document).ready(function(){
+  // sorting out the height and width of the div
+  position_div = $("div.container");
+  divWidth = position_div.width();
+  divHeight = position_div.height();
+  // position_div.height(divHeight);
+
+  // finding the images
+  $(".container img").each(function(){
+    let work = new Work($(this));
+    works.push(work);
+    work.choosePosition();
   });
 });
