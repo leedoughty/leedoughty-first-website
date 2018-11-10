138e181133b40ed302daa4ad5b698da49e46189b
Lee Doughty <leedoughtydesign@gmail.com>
2018-11-10 09:57:18 +0000

    Update images and layout for work pages

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,9 +1,22 @@
+// let imageNames = ['../images/preload/preload3.gif', '../images/preload/preload2.gif'];
+// let images = [];
+//
+// imageNames.forEach(function(name){
+//   image = new Image();
+//   image.src = name;
+//   images.push(image);
+// })
+
 let canvas = document.querySelector('#viewport');
+
+canvas.width = window.innerWidth/2;
+canvas.height = window.innerHeight;
+
 let ctx = canvas.getContext('2d');
 
-
-make_base('../images/preload/work2.jpg');
-make_base('../images/preload/work1.jpg');
+// make_base('../images/preload/preload3.gif');
+// make_base('../images/preload/preload2.gif');
+// make_base()
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
